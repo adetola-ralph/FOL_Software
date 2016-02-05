910ed3c1a60eec958be41742e9b7df8d1079ae38
Olutola Oreofeoluwapo <oreofe.olutola@gmail.com>
2016-02-05 16:43:16 +0100

    First application commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+      <title> Test index.php page</title>
+    </head>
+    <body>
+      <p>Test index.php page, haven't decided on the structure of the site</p>
+    </body>
+</html>
