727514338ea135bf6a3bdbc71d4695e80f4da272
Olutola Oreofeoluwapo <oreofe.olutola@gmail.com>
2016-02-11 17:05:15 +0100

    header and footer html file DRY

---
+++ layout/footer.php
@@ -0,0 +1,7 @@
+<?php
+
+echo("
+<script src='../js/js.js' type='text/javascript'></script>
+</body>
+</html>");
+?>
---
+++ layout/header.php
@@ -0,0 +1,11 @@
+<?php
+echo("<!DOCTYPE html>
+<html>
+<head>
+<title>Untitled Document</title>
+<link href='../style/style.css' rel='stylesheet' type='text/css' />
+
+</head>
+
+<body>");
+?>
