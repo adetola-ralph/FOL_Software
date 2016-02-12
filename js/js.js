19c847eb2cda99f8cf0f405f29c9dd977b04edfd
Olutola Oreofeoluwapo <oreofe.olutola@gmail.com>
2016-02-12 21:19:43 +0100

    added comments outlining work to be done

---
+++ js/js.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	/*
+		1. load the country table into a dropdown box from mysql db
+		2. load the age groups into the dropdown box from mysql db
+		3. 
+	*/
+});
+
+//ajax function that deals with correct postcode format
+//ajax code that gets the addresses from postcode and inserts them into the address bar
+//js code to allow for others in the title box
+//js code to see if the required fields are filled
+//js code that catches submit and uses all above functions for validation
