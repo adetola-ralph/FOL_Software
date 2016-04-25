9e3163a16976b7d9b0cd65291b1a3cfc9ce58f28
Olutola Oreofeoluwapo <oreofe.olutola@gmail.com>
2016-04-25 10:05:02 +0100

    used glyphicons instead of images in the buttons, modified buttons to centre themselves in a mobile view

--- index.php
+++ index.php
@@ -4,46 +4,50 @@
 ?>
     <div class="row">
     	<div class="col-md-4">
-        	<!--<div class="row">
-            	<div class="col-sm-4">
+        	<div class="row" style="padding-bottom:10px;">
+            	<div class="col-xs-4">
         		</div>
-                <div class="col-sm-4">-->
-                    <a href="data_input.php" class="btn btn-lg btn-success" role="button">
-                        <img src="images/add-contact.png" width="100" height="100" class="img-responsive img-thumbnail"/>
-                        
-                    </a><p class="align-center">Add new Convert</p>
-                <!--</div>
-                <div class="col-sm-4">
+                <div class="col-xs-4">
+                    <a href="data_input.php" class="btn btn-lg btn-success" role="button" >
+                        <!--<img src="images/add-contact.png" width="100" height="100" class="img-responsive img-thumbnail"/>-->
+                        <i class="glyphicon glyphicon-user gi-5x" aria-hidden="true"></i>
+                        <p class="align-center">Add new Convert</p>
+                    </a>
+                    
                 </div>
-        	</div>-->
+                <div class="col-xs-4">
+                </div>
+        	</div>
         </div>
         <div class="col-md-4">
-        	<!--<div class="row">
-            	<div class="col-sm-4">
+        	<div class="row" style="padding-bottom:10px;">
+            	<div class="col-xs-4">
         		</div>
-                <div class="col-sm-4">-->
+                <div class="col-xs-4">
                 	<a href="print.php" class="btn btn-lg btn-success" role="button">
-                		<img src="images/printer.png" width="100" height="100" class="img-responsive img-thumbnail" />
-                        
-                    </a><p class="align-center">Print Converts in CSV file</p>
-                <!--</div>
-                <div class="col-sm-4">
+                		<!--<img src="images/add-contact.png" width="100" height="100" class="img-responsive img-thumbnail"/>-->
+                        <i class="glyphicon glyphicon-print gi-5x" aria-hidden="true"></i>
+                        <p class="align-center">Print CSV file</p>
+                    </a>
                 </div>
-        	</div>-->
+                <div class="col-xs-4">
+                </div>
+        	</div>
         </div>
         <div class="col-md-4">
-        	<!--<div class="row">
-            	<div class="col-sm-4">
+        	<div class="row" style="padding-bottom:10px;">
+            	<div class="col-xs-4">
         		</div>
-                <div class="col-sm-4">-->
+                <div class="col-xs-4">
                 	<a href="#" class="btn btn-lg btn-success" role="button">
-                        <img src="images/statistics.png" width="100" height="100" class="img-responsive img-thumbnail"/>
-                        
-                    </a><p class="align-center">See statistics</p>
-               <!-- </div>
-                <div class="col-sm-4">
+                        <!--<img src="images/add-contact.png" width="100" height="100" class="img-responsive img-thumbnail"/>-->
+                        <i class="glyphicon glyphicon-stats gi-5x" aria-hidden="true"></i>
+                        <p class="align-center">See statistics</p>
+                    </a>
+               </div>
+                <div class="col-xs-4">
                 </div>
-        	</div>-->
+        	</div>
         </div>    	    
     </div>
 <?php include_once("layout/footer.php");?>
